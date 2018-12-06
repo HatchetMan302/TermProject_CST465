5b0f85c927cc0dfa850ee6b6e3c4493a8585f24f
Paul Simonsen <paul.simonsen@oit.edu>
2018-12-06 01:51:42 -0800

    Misc fixes

--- Term_Project/Controllers/HomeController.cs
+++ Term_Project/Controllers/HomeController.cs
@@ -17,14 +17,7 @@ namespace Term_Project.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Marble It Up! is the joyful, bouncing, beautiful game you’ve been waiting for – and it’s available NOW!";
 
             return View();
         }
